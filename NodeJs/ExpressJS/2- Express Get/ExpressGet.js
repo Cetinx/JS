14f7cc438a9b706000d56918f1311d2b74dd2701
Cetinx <82613065+Cetinx@users.noreply.github.com>
2021-12-03 13:26:12 +0300

    Express Get
    ExpressJS

---
+++ NodeJs/ExpressJS/2- Express Get/ExpressGet.js
@@ -0,0 +1,17 @@
+//npm i express
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res) => {
+
+    res.send('Merhaba')
+
+})
+
+
+
+
+
+app.listen(3000)
+console.log("Express Başlatıldı");
